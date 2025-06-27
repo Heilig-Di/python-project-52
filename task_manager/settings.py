9930c5fcf724c82b64a7df3a84453525d273ac48
Диана <heilig1996@mail.ru>
2025-06-27 16:04:41 +0300

    add .

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-g8i9qfkkjpf)c4=!g^31t_xm2)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1:8000', 'localhost', 'hexlet-code-x97y.onrender', '*']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost', 'hexlet-code-x97y.onrender']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
