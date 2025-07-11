8d03e75aa555d937ea71b5ebed26ebf1510c293a
Диана <heilig1996@mail.ru>
2025-07-11 15:18:59 +0300

    add

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,78 @@
 from django.test import TestCase
+from .models import User
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            first_name='Test',
+            last_name='User',
+            password='password123'
+        )
+
+        self.other_user = User.objects.create_user(
+            username='otheruser',
+            first_name='Other',
+            password='123password'
+        )
+
+
+    def test_user_create(self):
+        response = self.client.post(reverse('users:create'), {
+            'username': 'newuser',
+            'first_name': 'Tom',
+            'last_name': 'User',
+            'password1': 'newpassword',
+            'password2': 'newpassword',
+        })
+        self.assertRedirects(response, reverse("login"))
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+        user = User.objects.get(username="newuser")
+        self.assertEqual(user.first_name, "Tom")
+        self.assertEqual(user.last_name, "User")
+
+
+    def test_user_update(self):
+        self.client.login(username='testuser', password='password123')
+
+        response = self.client.post(
+            reverse('users:update', args=[self.user.pk]),
+            {'first_name': 'Jerry',
+             'last_name': 'User',
+        })
+        self.assertRedirects(response, reverse("users:list"))
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, 'Jerry')
+        self.assertEqual(self.user.last_name, 'User')
+
+
+    def test_other_update(self):
+        self.client.login(username='testuser', password='password123')
+
+        original_name = self.other_user.first_name
+        response = self.client.post(
+            reverse('users:update', args=[self.other_user.pk]),
+            {'first_name': 'Tuffy'}
+        )
+        self.other_user.refresh_from_db()
+        self.assertNotEqual(self.other_user.first_name, original_name)
+
+
+    def test_user_delete(self):
+        self.client.login(username='testuser', password='password123')
+
+        response = self.client.post(reverse('users:delete', args=[self.user.pk]))
+        self.assertRedirects(response, reverse('users:list'))
+        self.assertFalse(User.objects.filter(username='testuser').exists())
+
+
+    def test_other_delete(self):
+        self.client.login(username='testuser', password='password123')
+        self.client.post(reverse('users:delete', args=[self.other_user.pk]))
+        self.assertTrue(User.objects.filter(username='testuser').exists())
