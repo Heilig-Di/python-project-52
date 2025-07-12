0ec6c9ad6b360019cbd8fce5edcf0e4a15c30be9
Диана Захарчук <heilig1996@mail.ru>
2025-07-12 20:22:53 +0300

    Update models.py

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class User(AbstractUser):
 
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
