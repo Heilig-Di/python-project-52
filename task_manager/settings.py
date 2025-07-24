86025af8932b07f4699ce09113c31bf5ab0875d9
Диана <heilig1996@mail.ru>
2025-07-24 17:37:38 +0300

    add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,13 +94,10 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
