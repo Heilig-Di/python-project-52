4cf0f01b11ddfd07632814bbda686faa1428c1b7
Диана <heilig1996@mail.ru>
2025-07-24 20:09:31 +0300

    add

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -2,11 +2,20 @@ from django import forms
 from .models import Task
 from task_manager.statuses.models import Status
 from task_manager.users.models import User
+from django.utils.translation import gettext_lazy as _
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
+        labels = {
+            'name': _('Имя'),
+            'description': _('Описание'),
+            'status': _('Статус'),
+            'executor': _('Исполнитель'),
+            'labels': _('Метки'),
+        }
+
         widgets = {
             'labels': forms.SelectMultiple(),
         }
