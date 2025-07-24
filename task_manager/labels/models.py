a5a96f8a4a5eb7e6c6cecc7daf8555d6ac3e7e9e
Диана <heilig1996@mail.ru>
2025-07-24 21:14:36 +0300

    add

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Label(models.Model):
-    name = models.CharField(max_length=100, unique=True, name=_('Имя'))
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('Имя'))
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
