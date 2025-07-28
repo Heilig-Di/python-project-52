b576e633a952a535c15b9d47be5a60f239be9475
Диана <heilig1996@mail.ru>
2025-07-28 10:59:59 +0300

    add

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -6,7 +6,13 @@ from .models import User
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'pass1', 'pass2']
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2'
+        ]
         labels = {
             'username': 'Имя пользователя',
             'first_name': 'Имя',
@@ -30,12 +36,18 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'pass1', 'pass2']
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2'
+        ]
 
     def clean(self):
         cleaned_data = super().clean()
-        password1 = cleaned_data.get("pass1")
-        password2 = cleaned_data.get("pass2")
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
 
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Введенные пароли не совпадают")
@@ -44,7 +56,7 @@ class UserUpdateForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        password = self.cleaned_data["pass1"]
+        password = self.cleaned_data["password1"]
         if password:
             user.set_password(password)
         if commit:
